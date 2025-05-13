aef38c35bad134d2ed67e06996f30690f1bac096
Vishal Pratap Singh <152519316+VIRAT178@users.noreply.github.com>
2025-05-13 11:36:03 +0530

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -6,7 +6,6 @@ const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
 const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
 const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 
-
 export default{
     JWT_USER_PASSWORD,
     JWT_ADMIN_PASSWORD,
